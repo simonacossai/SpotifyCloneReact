08a53b3c006b4343e7561ddd2efe6410f1fac42f
HolaSoyGonzalo <72199035+HolaSoyGonzalo@users.noreply.github.com>
2021-02-05 14:39:20 +0100

    REVERT THIS DICK

--- src/components/Home/Gallery.jsx
+++ src/components/Home/Gallery.jsx
@@ -1,34 +1,22 @@
 import React from "react";
 import { Row, Col, Spinner } from "react-bootstrap";
-import "./Gallery.css";
-import { connect } from "react-redux";
+import './Gallery.css';
 
 import Song from "./Song";
-const mapStateToProps = (state) => state;
 
-const Gallery = (propsX) => {
-  console.log("Gallery ", propsX.songsArray.eminemSongs.slice(0, 5));
-  console.log("TITLE", propsX.title);
+const Gallery = ({ title, songs, loading, props }) => {
   return (
     <div id="song mb-5 ">
-      <h3 className="text-white text-left" style={{ marginLeft: "270px" }}>
-        {propsX.title}
-      </h3>
-      <Row className="row-cols-sm-2 row-cols-lg-4 row-cols-xl-6 mb-4 no-gutters justify-content-end song-row">
-        {propsX.title === "#THROWBACKTHURSDAY" &&
-          propsX.songsArray.eminemSongs
-            .slice(0, 5)
-            .map((song) => <Song song={song} />)}
-        {propsX.title === "#TOP50ITALY" &&
-          propsX.songsArray.arianaGrandeSongs
-            .slice(0, 5)
-            .map((song) => <Song song={song} />)}
-        {propsX.title === "#TRENDINGNOW" &&
-          propsX.songsArray.museSongs
-            .slice(0, 5)
-            .map((song) => <Song song={song} />)}
+      <h3 className="text-white text-left" style={{marginLeft:"270px"}}>{title}</h3>
+      <Row className="row-cols-sm-2 row-cols-lg-4 row-cols-xl-6 mb-4 no-gutters justify-content-end song-row" >
+        {loading
+          ?
+              <Col className="d-flex align-items-center justify-content-center text-center mx-auto">
+                <Spinner animation="border" variant="light" />
+              </Col>
+          : songs.map((song) => <Song data={song} key={song.id} props={props}/>)}
       </Row>
     </div>
   );
 };
-export default connect(mapStateToProps)(Gallery);
+export default Gallery;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,116 +1,57 @@
 import React, { Component } from "react";
 import Gallery from "./Gallery";
-import "./Gallery.css";
-import { connect } from "react-redux";
+import './Gallery.css';
+
 import { Alert } from "react-bootstrap";
 
-//START OF REDUX
-
-const mapStateToProps = (state) => state;
-const mapDispatchToProps = (dispatch) => ({
-  addDataToArtist: async (artistName) =>
-    dispatch(async (dispatch, getState) => {
-      let response = await fetch(
-        "https://deezerdevs-deezer.p.rapidapi.com/search?q=" + artistName,
-        {
-          method: "GET",
-          headers: {
-            "x-rapidapi-key":
-              "ec5577de62msh9d203d454724b43p1f3c08jsnad1a59a31f6f",
-            "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
-          },
-        }
-      );
-      let data = await response.json();
-      console.log("DATA ->" )
-      console.log(data.data);
-      console.log("<- DATA" )
-      let songs = data.data
-      if (artistName === "eminem") {
-        dispatch({
-          type: "ADD_SONGS_TO_EMINEM",
-          payload: songs,
-        });
-      } else if (artistName === "muse") {
-        dispatch({
-          type: "ADD_SONGS_TO_MUSE",
-          payload: songs,
-        });
-      } else if (artistName === "ariana") {
-        dispatch({
-          type: "ADD_SONGS_TO_ARIANA",
-          payload: songs,
-        });
-      }
-    }),
-  startLoading: () =>
-    dispatch({
-      type: "START_LOADING",
-    }),
-  stopLoading: () =>
-    dispatch({
-      type: "STOP_LOADING",
-    }),
-});
-
-//END OF REDUX
 class Home extends Component {
   state = {
     eminemSongs: [],
     museSongs: [],
-    arianaGrandeSongs: [],
+   arianaGrandeSongs: [],
     loading: true,
     error: false,
   };
 
   url = "https://deezerdevs-deezer.p.rapidapi.com/search?q=";
 
-  fetchsongs = async () => {
-    await this.props.addDataToArtist("eminem");
-    await this.props.addDataToArtist("muse");
-    await this.props.addDataToArtist("ariana");
-    console.log(this.props.songsArray.eminemSongs);
-    console.log("PROPS->",this.props )
-    console.log()
-
-    await Promise.all([
-      fetch(this.url + "eminem", {
-        method: "GET",
-        headers: {
-          "x-rapidapi-key":
-            "ec5577de62msh9d203d454724b43p1f3c08jsnad1a59a31f6f",
-          "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
-        },
+  fetchsongs = () => {
+    Promise.all([
+      fetch(this.url + "eminem",{
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-key": "ec5577de62msh9d203d454724b43p1f3c08jsnad1a59a31f6f",
+            "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com"
+        }
       })
         .then((response) => response.json())
         .then((responseObject) => {
           this.setState({ eminemSongs: responseObject.data }, () =>
-            console.log("not ours",this.state.eminemSongs)
+            console.log(this.state.eminemSongs)
           );
         }),
-      fetch(this.url + "muse", {
-        method: "GET",
-        headers: {
-          "x-rapidapi-key":
-            "ec5577de62msh9d203d454724b43p1f3c08jsnad1a59a31f6f",
-          "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
-        },
+      fetch(this.url + "muse",{
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-key": "ec5577de62msh9d203d454724b43p1f3c08jsnad1a59a31f6f",
+            "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com"
+        }
       })
         .then((response) => response.json())
         .then((responseObject) =>
           this.setState({ museSongs: responseObject.data })
+         
         ),
-      fetch(this.url + "halsey", {
-        method: "GET",
-        headers: {
-          "x-rapidapi-key":
-            "ec5577de62msh9d203d454724b43p1f3c08jsnad1a59a31f6f",
-          "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
-        },
+      fetch(this.url + "halsey",{
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-key": "ec5577de62msh9d203d454724b43p1f3c08jsnad1a59a31f6f",
+            "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com"
+        }
       })
         .then((response) => response.json())
         .then((responseObject) =>
-          this.setState({ arianaGrandeSongs: responseObject.data })
+          this.setState({arianaGrandeSongs: responseObject.data })
         ),
     ])
       .then(() => this.setState({ loading: false }))
@@ -120,64 +61,75 @@ class Home extends Component {
       });
   };
 
-  componentDidMount=async() => {
-    await this.fetchsongs();
-    console.log("BIG PROBLEM",this.props)
-    console.log("from state",this.state.eminemSongs)
+  componentDidMount() {
+    this.fetchsongs();
   }
 
   render() {
     return (
       <div>
         <div className="container-fluid mb-5">
+  
           {this.state.error && (
             <Alert variant="danger" className="text-center">
               An error has occurred, please try again later
             </Alert>
           )}
 
-          {!this.state.error && (
-            <>
-              <div className="container mb-4 text-center d-flex mx-auto justify-content-center">
-                <nav aria-label="breadcrumb ml-5">
-                  <ol className="breadcrumb ml-5">
-                    <li className="breadcrumb-item ml-3">
-                      <a href="#">Trending</a>
-                      <div className="underbar"></div>
-                    </li>
-                    <li className="breadcrumb-item">
-                      <a href="#">Podcast</a>
-                      <div className="underbar"></div>
-                    </li>
-                    <li className="breadcrumb-item">
-                      <a href="#">Moods and genres</a>
-                      <div className="underbar"></div>
-                    </li>
-                    <li className="breadcrumb-item">
-                      <a href="#">New releases</a>
-                      <div className="underbar"></div>
-                    </li>
-                    <li className="breadcrumb-item">
-                      <a href="#">Discover</a>
-                      <div className="underbar"></div>
-                    </li>
-                  </ol>
-                </nav>
-              </div>
-              <Gallery
-                title="#THROWBACKTHURSDAY"
-              />
-              <Gallery
-                title="#TOP50ITALY"
-              />
-              <Gallery
-                title="#TRENDINGNOW"
-              />
-            </>
-          )}
+     
+          {!this.state.error &&
+           
+              <>
+      <div className="container mb-4 text-center d-flex mx-auto justify-content-center">
+      <nav aria-label="breadcrumb ml-5">
+        <ol className="breadcrumb ml-5">
+          <li className="breadcrumb-item ml-3">
+            <a href="#">Trending</a>
+            <div className="underbar"></div>
+          </li>
+          <li className="breadcrumb-item">
+            <a href="#">Podcast</a>
+            <div className="underbar"></div>
+          </li>
+          <li className="breadcrumb-item">
+            <a href="#">Moods and genres</a>
+            <div className="underbar"></div>
+          </li>
+          <li className="breadcrumb-item">
+            <a href="#">New releases</a>
+            <div className="underbar"></div>
+          </li>
+          <li className="breadcrumb-item">
+            <a href="#">Discover</a>
+            <div className="underbar"></div>
+          </li>
+        </ol>
+      </nav>
+    </div>
+                <Gallery
+                  title="#THROWBACKTHURSDAY"
+                  loading={this.state.loading}
+                  songs={this.state.museSongs.slice(0, 5)}
+                  props={this.props} 
+                />
+                <Gallery
+                  title="#TOP50ITALY"
+                  loading={this.state.loading}
+                  songs={this.state.arianaGrandeSongs.slice(0, 5)}
+                  props={this.props} 
+                />
+                <Gallery
+                  title="#TRENDINGNOW"
+                  loading={this.state.loading}
+                  songs={this.state.eminemSongs.slice(0, 5)}
+                  style={{marginBottom: "100px"}}
+                  props={this.props} 
+                />
+              </>
+            }
         </div>
       </div>
     );
   }
 }
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
+export default Home;
